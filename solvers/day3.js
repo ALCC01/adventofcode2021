c283c19e4c38bb2cddd58d9e976a28b9b4c0d8ea
Alberto Coscia <alberto@albertocoscia.me>
2021-12-03 09:28:02 +0100

    Add solver for 3.2

--- solvers/day3.js
+++ solvers/day3.js
@@ -2,17 +2,44 @@ export default (part, input) => {
   input = input.toString().split('\n')
     .map(e => e.split('').map(e => parseInt(e)))
 
-  const values = input.reduce((t, e, i, a) => {
+  if (part === 1) {
+    const values = common(input)
+    // If a position is > 0 then 1 is the most common value
+    const gamma = arrayToNumber(values.map(e => e > 0 ? 1 : 0))
+    // If a position is > 0 then 0 is the least common value
+    const epsilon = arrayToNumber(values.map(e => e > 0 ? 0 : 1))
+
+    return gamma * epsilon
+  } else {
+    let [o2, co2, o2i, co2i] = [input, input, 0, 0]
+
+    // Apply criteria until we have a single number
+    while (o2.length > 1) {
+      o2 = criteria(true, o2, o2i)
+      o2i++
+    }
+
+    while (co2.length > 1) {
+      co2 = criteria(false, co2, co2i)
+      co2i++
+    }
+
+    return arrayToNumber(co2[0]) * arrayToNumber(o2[0])
+  }
+}
+
+const arrayToNumber = (array) => parseInt(array.join(''), 2)
+
+function criteria (o2, input, i) {
+  const values = common(input)
+  return input.filter(e => e[i] === (o2 ? +(values[i] >= 0) : +(values[i] < 0)))
+}
+
+function common (input) {
+  return input.reduce((t, e, i, a) => {
     e.forEach((b, i) => {
       t[i] += b - 0.5
     })
     return t
   }, new Array(input[0].length).fill(0))
-
-  // If a position is > 0 then 1 is the most common value
-  const gamma = parseInt(values.map(e => e > 0 ? 1 : 0).join(''), 2)
-  // If a position is > 0 then 0 is the least common value
-  const epsilon = parseInt(values.map(e => e > 0 ? 0 : 1).join(''), 2)
-
-  return gamma * epsilon
 }
