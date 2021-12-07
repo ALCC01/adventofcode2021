2815e32184deb94454f5b7816df572ad50e07f8f
Alberto Coscia <alberto@albertocoscia.me>
2021-12-07 10:20:52 +0100

    Add solver for 7.2

--- solvers/day7.js
+++ solvers/day7.js
@@ -1,8 +1,17 @@
 export default (part, input) => {
+  const p2 = part === 2
   input = input.toString().split(',').map(Number).filter(e => !isNaN(e)).sort((a, b) => a - b)
 
+  let target = 0
+  // Use the median for part 1
   // This assumes that input.length is even
-  const target = input[(input.length) / 2]
+  if (!p2) target = input[(input.length) / 2]
+  // Use the average for part 2
+  else target = Math.floor(input.reduce((t, e) => t + e, 0) / input.length)
 
-  return input.map(e => Math.abs(e - target)).reduce((t, e) => t + e, 0)
+  return input.map(e => !p2
+    ? Math.abs(e - target)
+    // Sum of consecutive numbers n(n+1)/2
+    : Math.abs(e - target) * (Math.abs(e - target) + 1) / 2
+  ).reduce((t, e) => t + e, 0)
 }
