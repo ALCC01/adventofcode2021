751ee7a0e75a55f558e4fd3136df7549a4a9c3c0
Alberto Coscia <alberto@albertocoscia.me>
2021-12-10 09:14:36 +0100

    Add solver for 10.2

--- solvers/day10.js
+++ solvers/day10.js
@@ -1,31 +1,39 @@
 export default (part, input) => {
   input = input.toString().split('\n').filter(e => e).map(e => e.split(''))
 
-  const scores = input.map(line => {
+  let scores = input.map(line => {
     const stack = []
+    // Return a positive score for corrupt lines
     for (const c of line) {
       if (closingChars.indexOf(c) === -1) stack.push(c)
-      else if (!compare(stack.pop(), c)) return values[c]
+      else if (c !== equiv[stack.pop()]) return values[c][0]
     }
+
+    // Return a negative score for incomplete lines
+    if (part === 2 && stack.length !== 0) return stack.reverse().reduce((t, c) => t * 5 - values[equiv[c]][1], 0)
 
     return 0
   })
 
-  return scores.reduce((t, e) => t + e, 0)
+  if (part === 1) return scores.filter(e => e >= 0).reduce((t, e) => t + e, 0)
+
+  // Invert negative scores for incomplete lines
+  scores = scores.filter(e => e <= 0).sort((a, b) => a - b)
+  return -scores[(scores.length - 1) / 2]
 }
 
 const values = {
-  ')': 3,
-  ']': 57,
-  '}': 1197,
-  '>': 25137
+  ')': [3, 1],
+  ']': [57, 2],
+  '}': [1197, 3],
+  '>': [25137, 4]
 }
 
 const closingChars = Object.keys(values)
 
-const compare = (a, b) => {
-  return (a === '(' && b === ')') ||
-    (a === '[' && b === ']') ||
-    (a === '{' && b === '}') ||
-    (a === '<' && b === '>')
+const equiv = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>'
 }
