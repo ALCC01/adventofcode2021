0b0d7f34c8932c80883a49733b5d2bc2ba2afe6d
Alberto Coscia <alberto@albertocoscia.me>
2021-12-13 09:15:33 +0100

    Add solver for 13.2

--- solvers/day13.js
+++ solvers/day13.js
@@ -5,11 +5,26 @@ export default (part, input) => {
     return [points, folds]
   }, [[], []])
 
-  // JS array comparison sucks
-  return new Set(fold(points, folds[0]).map(String)).size
+  if (part === 1) return new Set(fold(points, folds[0]).map(String)).size
+
+  return printGrid(folds.reduce((p, f) => fold(p, f), points))
 }
 
 const fold = (points, [ax, i]) => points.map(([x, y]) => [
   ax === 'x' ? i - Math.abs(x - i) : x,
   ax === 'y' ? i - Math.abs(y - i) : y
 ])
+
+const printGrid = (points) => {
+  const mX = points.sort((a, b) => b[0] - a[0])[0][0]
+  const mY = points.sort((a, b) => b[1] - a[1])[0][1]
+  let r = ''
+
+  points = new Set(points.map(String))
+  for (let y = 0; y <= mY; y++) {
+    for (let x = 0; x <= mX; x++) r += points.has(`${x},${y}`) ? '█' : ' '
+    r += '\n'
+  }
+
+  return r
+}
