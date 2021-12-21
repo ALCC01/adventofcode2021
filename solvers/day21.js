1399096cc95ff3636f20dcbd29ba32b01527a3d4
Alberto Coscia <alberto@albertocoscia.me>
2021-12-21 22:25:41 +0100

    Add solver for 21.2

--- solvers/day21.js
+++ solvers/day21.js
@@ -2,6 +2,8 @@ export default (part, input) => {
   let [p1, p2] = input.toString().trim().split('\n').map(e => +e.slice(28))
   let [score1, score2] = [0, 0]
 
+  if (part === 2) return Math.max(...wins(p1, p2, 0, 0, true))
+
   const die = dice()
   while (true) {
     let [roll, n] = die.next().value
@@ -25,3 +27,46 @@ function * dice () {
     n += 3
   }
 }
+
+// Maps each possible roll result to the number of its occurences
+// Eg 4 can be the result of 3 different permutations of 3 dice rolls
+const rollCounts = [
+  [3, 1],
+  [4, 3],
+  [5, 6],
+  [6, 7],
+  [7, 6],
+  [8, 3],
+  [9, 1]
+]
+
+// Memory is to memorization as memoy is to memoization ;)
+// Caches the number of wins for a given set of conditions
+const memoy = []
+
+const wins = (p1, p2, score1, score2, isP1) => {
+  const key = [score1, p1, score2, p2, isP1]
+  let res = [0, 0]
+
+  if (memoy[key]) return memoy[key]
+  else if (score1 >= 21) res = [1, 0]
+  else if (score2 >= 21) res = [0, 1]
+  else {
+    for (const [roll, n] of rollCounts) {
+      if (isP1) {
+        const pos = (p1 + roll - 1) % 10 + 1
+        res = sum(res, mul(wins(pos, p2, score1 + pos, score2, false), n))
+      } else {
+        const pos = (p2 + roll - 1) % 10 + 1
+        res = sum(res, mul(wins(p1, pos, score1, score2 + pos, true), n))
+      }
+    }
+  }
+
+  return memoy[key] = res // eslint-disable-line no-return-assign
+}
+
+// Sums two pairs of values
+const sum = ([a, c], [b, d]) => [a + b, c + d]
+// Multiplies a pair of values by another value
+const mul = ([a, b], c) => [a * c, b * c]
